9ab870f8dcf3db59364fcd2f76c75432b92a8230
William Gathoye <william@gathoye.be>
2018-10-21 20:34:58 +0200

    Use english based role name to keep consistency across project

--- 1_basis/create_role_and_users.sql
+++ 1_basis/create_role_and_users.sql
@@ -1,19 +1,19 @@
-create role sgbd2g_role not identified;
-grant alter session to sgbd2g_role;
-grant create session to sgbd2g_role;
-grant create table to sgbd2g_role;
-grant create procedure to sgbd2g_role;
-grant create sequence to sgbd2g_role;
-grant create trigger to sgbd2g_role;
-grant create type to sgbd2g_role;
-grant create view to sgbd2g_role;
-grant execute on sys.owa_opt_lock to sgbd2g_role;
-grant execute on sys.dbms_lock to sgbd2g_role;
+create role dbms2_role not identified;
+grant alter session to dbms2_role;
+grant create session to dbms2_role;
+grant create table to dbms2_role;
+grant create procedure to dbms2_role;
+grant create sequence to dbms2_role;
+grant create trigger to dbms2_role;
+grant create type to dbms2_role;
+grant create view to dbms2_role;
+grant execute on sys.owa_opt_lock to dbms2_role;
+grant execute on sys.dbms_lock to dbms2_role;
 
-create user kata identified by oracle default tablespace users account unlock;
-alter user kata quota unlimited on users;
-grant sgbd2g_role to kata;
+create user dbms2_kata identified by oracle default tablespace users account unlock;
+alter user dbms2_kata quota unlimited on users;
+grant dbms2_role to dbms2_kata;
 
-create user sakila identified by oracle default tablespace users account unlock;
-alter user sakila quota unlimited on users;
-grant sgbd2g_role to sakila;
+create user dbms2_sakila identified by oracle default tablespace users account unlock;
+alter user dbms2_sakila quota unlimited on users;
+grant dbms2_role to dbms2_sakila;
